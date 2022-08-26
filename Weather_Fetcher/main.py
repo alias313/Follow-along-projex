89a729466d36ec8e3e44ea6752e43e6dc7ea6704
ele-is <eliesgarciaalvira@gmail.com>
2022-08-26 19:38:50 +0200

    WF completed

--- Weather_Fetcher/main.py
+++ Weather_Fetcher/main.py
@@ -1,3 +1,6 @@
+"""Fetches weather data from OpenWeather's API"""
+
+from doctest import ELLIPSIS_MARKER
 import json
 import requests
 
@@ -11,3 +14,11 @@ key = saved_key["key"]
 
 city = input("Enter a city name")
 
+request_url = f"{BASE_URL}?appid={key}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("An error ocurred.")
