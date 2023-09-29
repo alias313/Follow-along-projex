213cb3f5d5f35c3ccb0d50a1d1d25b59dc1f7a1f
alias313 <eliesgarciaalvira@gmail.com>
2023-09-30 00:08:09 +0200

    Renamed again to not confuse future self

--- Django_Rest_Framework_Books_Project/Django_Rest_Framework_Books_Project/urls.py
+++ Django_Rest_Framework_Books_Project/Django_Rest_Framework_Books_Project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for books project.
+URL configuration for Django_Rest_Framework_Books_Project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
