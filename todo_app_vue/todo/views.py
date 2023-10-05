de411c18921d19de08728fb817f52bbd1f2886b1
alias313 <eliesgarciaalvira@gmail.com>
2023-10-05 13:08:31 +0200

    Added class TodoSerializer

--- todo_app_vue/todo/views.py
+++ todo_app_vue/todo/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Todo
+from .serializer import TodoSerializer
 
-# Create your views here.
+class TodoView(APIView):
+
+    def get(self, request):
+        user = request.user
+        todos = Todo.objects.filter(user = user)
+        serializer = TodoSerializer(todos, many = True)
+        return Response({
+            'status': True,
+            'data':  serializer.data,
+            'message': 'todo fetched succesfully'
+        })
+
+    def post(self, request):
+        try:
+            user = request.user
+            data = request.data
+            data['user'] = user.uid
+            serializer = TodoSerializer(data = data)
+
+            id serializer.is_valid():
+                return Response({
+                    'status': False,
+                    'message': 'invalid fields',
+                    'data': serializer.errors
+                }) 
+
+            serializer.save()
+            return Response({
+                'status': True,
+                'message': 'Todo is created',
+                'data': serializer.data
+            })
