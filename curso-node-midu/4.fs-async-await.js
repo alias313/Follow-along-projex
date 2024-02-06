cae15b187ee6509c461373fe8794f65966b6287b
alias313 <eliesgarciaalvira@gmail.com>
2024-02-06 14:12:13 +0100

    Async node file reading

--- curso-node-midu/4.fs-async-await.js
+++ curso-node-midu/4.fs-async-await.js
@@ -1,16 +1,34 @@
+// Esto sólo en los módulos nativos
+// que no tienen promesas nativas
+
+// const { promisify } = require('node:util')
+// const readFilePromise = promisify(fs.readFile)
+
 const { readFile } = require('node:fs/promises')
 
-// IIFE - Immediately Invoked  Function Expression
-;(
-    async () => {
-        console.log('Leyendo el primer archivo ...')
-        const text = await readFile('./archivo.txt', 'utf-8')
-        console.log('primer texto:', text)
+async function init () {
+  console.log('Leyendo el primer archivo...')
+  const text = await readFile('./archivo.txt', 'utf-8')
+  console.log('primer texto:', text)
+  console.log('--> Hacer cosas mientras lee el archivo...')
+  
+  console.log('Leyendo el segundo archivo...')
+  const secondText = await readFile('./archivo2.txt', 'utf-8')
+  console.log('segundo texto:', secondText)    
+}
 
-        console.log('Hacer cosas mientras lee el primer archivo ...')
+init()
 
-        console.log('Leyendo el segundo archivo ...')
-        const secondText = await readFile('./archivo2.txt', 'utf-8') 
-        console.log('segundo texto:', secondText)
-    }
-)()
+// IIFE - Inmediatly Invoked Function Expression
+// ;(
+//   async () => {
+//     console.log('Leyendo el primer archivo...')
+//     const text = await readFile('./archivo.txt', 'utf-8')
+//     console.log('primer texto:', text)
+//     console.log('--> Hacer cosas mientras lee el archivo...')
+    
+//     console.log('Leyendo el segundo archivo...')
+//     const secondText = await readFile('./archivo2.txt', 'utf-8')
+//     console.log('segundo texto:', secondText)    
+//   }
+// )()
--- curso-node-midu/4.fs-promises.js
+++ curso-node-midu/4.fs-promises.js
@@ -1,19 +1,21 @@
+// Esto sólo en los módulos nativos
+// que no tienen promesas nativas
+
+// const { promisify } = require('node:util')
+// const readFilePromise = promisify(fs.readFile)
+
 const fs = require('node:fs/promises')
 
-// Solo en los modulos nativo que no tienen promesas nativas
-/* const { promisify } = require('node:util')
-const readFile = promisify(fs.readFile) */
-
-console.log('Leyendo el primer archivo ...')
+console.log('Leyendo el primer archivo...')
 fs.readFile('./archivo.txt', 'utf-8')
-    .then(text => {
-        console.log('primer texto:', text)
-    })
+  .then(text => {
+    console.log('primer texto:', text)
+  })
 
-console.log('Hacer cosas mientras lee el primer archivo ...')
+console.log('--> Hacer cosas mientras lee el archivo...')
 
-console.log('Leyendo el segundo archivo ...')
-fs.readFile('./archivo2.txt', 'utf-8') 
-    .then(text => {
-        console.log('segundo texto:', text)
-    })
+console.log('Leyendo el segundo archivo...')
+fs.readFile('./archivo2.txt', 'utf-8')
+  .then(text => {
+    console.log('segundo texto:', text)
+  })
