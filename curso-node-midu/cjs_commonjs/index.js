9409efddee694699a33dfe15bc913fa0752cbca7
alias313 <eliesgarciaalvira@gmail.com>
2024-02-07 01:39:20 +0100

    Deleted upstream folder duplicated files

--- curso-node-midu/cjs_commonjs/index.js
+++
@@ -1,7 +0,0 @@
-// CommonJS require module
-const sum = require('./sum')
-
-globalThis.console.log(typeof globalThis)
-
-console.log(sum(1, 2))
-
--- curso-node-midu/cjs_commonjs/sum.js
+++
@@ -1,9 +0,0 @@
-function sum (a, b) {
-    return a + b
-}
-
-// CommonJs expor module
-module.exports = { 
-    sum
-}
-
