ab434058ed22d711dccc5804a00908ec49b8a18b
alias313 <eliesgarciaalvira@gmail.com>
2024-04-12 23:59:43 +0200

    server code finished

--- git-http-server/server.ts
+++ git-http-server/server.ts
@@ -15,13 +15,37 @@ function requestToEnv(request: IncomingMessage): NodeJS.ProcessEnv {
     }
 }
 
+function stdoutToResponse(stdout: NodeJS.ReadableStream, response: ServerResponse) {
+    let isHeaderEnded = false;
+    stdout.on('data', (data) => {
+        if (isHeaderEnded) {
+            response.write(data)
+            return
+        }
+
+        const dataStr: string = data.toString('utf-8');
+        const [headers, ...body] = data.split('\r\n\r\n')
+
+        headers.split('\r\n').forEach((line) => {
+            const [key, value] = line.split(': ');
+            response.setHeader(key, value)
+        })
+
+        if (body.length > 0) {
+            isHeaderEnded = true;
+            response.write(body.join('\r\n\r\n'))
+        }
+
+        stdout.on('end', () => response.end())
+    })
+}
 
 createServer((request, response) => {
     const requestEnv = requestToEnv(request)
     const env = {
         ...requestEnv,
         GIT_HTTP_EXPORT_ALL: '',
-        GIT_PROJECT_ROOT: `/home/elies/Programming/Follow-along-projex/git-http-server`,
+        GIT_PROJECT_ROOT: `/home/elies/git-remote-repos`,
     };
 
     const backend = spawn('git-http-backend', { env })
