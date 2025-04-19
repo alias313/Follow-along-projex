3e58dc263a3189eeb1e307ecd878811e375ff792
Elies <eliesgarciaalvira@gmail.com>
2025-04-19 02:36:13 +0200

    This is actually cool I didn't know abt this

--- react/react-query/rq-av/src/App.tsx
+++ react/react-query/rq-av/src/App.tsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
+async function getAVDevices() {
+  try {
+    // This will trigger the permission prompt
+    await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+
+    return await navigator.mediaDevices.enumerateDevices();
+  } catch (error) {
+    console.error('Error accessing media devices:', error);
+    return await navigator.mediaDevices.enumerateDevices();
+  }}
+
 function App() {
-return (
-    <div>hi</div>
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['devices'],
+    queryFn: getAVDevices,
+  })
+
+  if (isLoading) return <div>Loading...</div>
+  if (isError || !data) return <div>Error</div> // Consider no data to be an error case
+
+  return (
+    <div>
+      {data.map((device) => (
+        <div key={`${device.kind}-${device.deviceId}`}>
+          {device.label || `${device.kind} (no label)`}
+        </div>
+      ))}
+    </div>
   );
 }
 
